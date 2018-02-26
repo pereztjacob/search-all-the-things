81c4d71fe73dfc221db0050ad8651a9770ee500d
pereztjacob <pereztjacob@gmail.com>
2018-02-26 12:46:46 -0800

    repair webpack config and add single book search to api file

--- src/services/bookApi.js
+++ src/services/bookApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
 const API_KEY = 'AIzaSyC15YhYq4uguEcPkzx7byzQQjKxiljDbuo';
 
 const throwJson = json => { throw json; };
@@ -8,7 +8,12 @@ const get = url => fetch(url)
 export function search({ topic }, page = 1, pageSize = 20) {
   let nextPage;
   page == 1 ? nextPage = page : nextPage = (page - 1) * pageSize;
-  const url = `${BASE_URL}${topic}&maxResults=${pageSize}&startIndex=${nextPage}&orderBy=newest&key=${API_KEY}`;
+  const url = `${BASE_URL}?q=${topic}&maxResults=${pageSize}&startIndex=${nextPage}&orderBy=newest&key=${API_KEY}`;
   console.log(url);
+  return get(url);
+}
+
+export function getBook(id){
+  const url = `${BASE_URL}/${id}?key=${API_KEY}`;
   return get(url);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   devServer: {
     contentBase: './${buildDir}',
+    historyApiFallback: true
   },
   devtool: 'inline-source-map',
   plugins: [
